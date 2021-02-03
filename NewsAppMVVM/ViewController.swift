0cb75012b46c0a965ac6463c36ea64df6fe12cbf
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2021-02-03 18:11:09 -0300

    Initial Commit

---
+++ NewsAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Marcio Habigzang Brufatto on 03/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
